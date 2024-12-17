052258e8059b783bd6fddf88c906228417f983cd
Alejandro Vasquez <alejandrocr713@hotmail.com>
2024-12-17 14:43:20 -0500

    conversor terminado

---
+++ src/CoversionResponse.java
@@ -0,0 +1,104 @@
+import com.google.gson.JsonObject;
+
+//aca se define la clase y sera util para mapear la estructura de la respuesta jacson.
+public class CoversionResponse {
+    private String result;
+    private String documentation;
+    private String terms_of_use;
+    private String time_last_update_unix;
+    private String time_last_update_utc;
+    private String time_next_update_unix;
+    private String time_next_update_utc;
+    private String base_code;
+    private String target_code;
+    private double conversion_rate;
+    private double conversion_result;
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
+    public String getDocumentation() {
+        return documentation;
+    }
+
+    public void setDocumentation(String documentation) {
+        this.documentation = documentation;
+    }
+
+    public String getTerms_of_use() {
+        return terms_of_use;
+    }
+
+    public void setTerms_of_use(String terms_of_use) {
+        this.terms_of_use = terms_of_use;
+    }
+
+    public String getTime_last_update_unix() {
+        return time_last_update_unix;
+    }
+
+    public void setTime_last_update_unix(String time_last_update_unix) {
+        this.time_last_update_unix = time_last_update_unix;
+    }
+
+    public String getTime_last_update_utc() {
+        return time_last_update_utc;
+    }
+
+    public void setTime_last_update_utc(String time_last_update_utc) {
+        this.time_last_update_utc = time_last_update_utc;
+    }
+
+    public String getTime_next_update_unix() {
+        return time_next_update_unix;
+    }
+
+    public void setTime_next_update_unix(String time_next_update_unix) {
+        this.time_next_update_unix = time_next_update_unix;
+    }
+
+    public String getTime_next_update_utc() {
+        return time_next_update_utc;
+    }
+
+    public void setTime_next_update_utc(String time_next_update_utc) {
+        this.time_next_update_utc = time_next_update_utc;
+    }
+
+    public String getBase_code() {
+        return base_code;
+    }
+
+    public void setBase_code(String base_code) {
+        this.base_code = base_code;
+    }
+
+    public String getTarget_code() {
+        return target_code;
+    }
+
+    public void setTarget_code(String target_code) {
+        this.target_code = target_code;
+    }
+
+    public double getConversion_rate() {
+        return conversion_rate;
+    }
+
+    public void setConversion_rate(double conversion_rate) {
+        this.conversion_rate = conversion_rate;
+    }
+
+    public double getConversion_result() {
+        return conversion_result;
+    }
+
+    public void setConversion_result(double conversion_result) {
+        this.conversion_result = conversion_result;
+    }
+}
